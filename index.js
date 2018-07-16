0092323c1e8e52a94f25e0e2f3b849a04ce9fb6f
Roberto Garza <robertg4567@hotmail.com>
2018-07-16 17:42:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
 function takeANumber(katzDeliLine, name){
-  katzDeliLine[...katDeliLine,name]
+  katzDeliLine=[...katDeliLine,name]
   return "Welcome, ${name}. you are numer ${katzDeliLine.length + 1} in line."
 }
