5ebf9e6a69ff1ef18a5136219710c04167d29237
Roberto Garza <robertg4567@hotmail.com>
2018-07-16 17:51:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function nowServing(katzDeliLine){
     return "There is nobody waiting to be served!"
   }
   else {
-    return "Currently serving "+katzDeliLine[0]+"."
+    return "Currently serving "+name+"."
   }
 }
