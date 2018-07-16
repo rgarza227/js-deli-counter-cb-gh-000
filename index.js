71e6a06939928260cbb196ae0563360aa51ffd43
Roberto Garza <robertg4567@hotmail.com>
2018-07-16 17:44:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
 function takeANumber(katzDeliLine, name){
   katzDeliLine=[...katzDeliLine,name]
-  return "Welcome, ${name}. you are numer ${katzDeliLine.length + 1} in line."
+  return "Welcome, " + name + ". you are number " + (katzDeliLine.length + 1)+" in line."
 }
