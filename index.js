b543141548aab1890b6d5bf6921ea513f92a5930
Roberto Garza <robertg4567@hotmail.com>
2018-07-16 17:50:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name)
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
+
+function nowServing(katzDeliLine){
+  katzDeliLine.slice(1)
+  if (katzDeliLine.length <= 1){
+    return "There is nobody waiting to be served!"
+  }
+  else {
+    return "Your turn"
+  }
+}
